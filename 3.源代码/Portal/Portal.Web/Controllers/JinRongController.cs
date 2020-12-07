c409816406354e7790f683ac447fd5b3b6dec583
shiliang <shiliang@idea-group.cn>
2020-12-07 14:02:04 +0800

    将PortalControler拆分成4个主页Controller
    Signed-off-by: shiliang <shiliang@idea-group.cn>

---
+++ 3.源代码/Portal/Portal.Web/Controllers/JinRongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Web.Controllers
+{
+    public class JinRongController : Controller
+    {
+        // GET: JinRong
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
