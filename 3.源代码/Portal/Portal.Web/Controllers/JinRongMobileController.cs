738cc67ff3f5c856ade56676a08e0cc8ee46ed99
Plantoms <508629733@qq.com>
2021-01-15 21:45:01 +0800

    小程序主页制作

--- 3.源代码/Portal/Portal.Web/Controllers/JinRongMobileController.cs
+++ 3.源代码/Portal/Portal.Web/Controllers/JinRongMobileController.cs
@@ -9,9 +9,51 @@ namespace Portal.Web.Controllers
     public class JinRongMobileController : Controller
     {
         // GET: JinRongMobile
+
+        #region 主页（视图）
         public ActionResult Index()
         {
             return View();
         }
+        #endregion
+
+        #region 按揭贷款（视图）
+        public ActionResult MortgageLoan()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 代办过户（视图）
+        public ActionResult ChargeTransfer()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 红本抵押（视图）
+        public ActionResult RedMortgage()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 现金赎楼（视图）
+        public ActionResult CashRedemptionFloor()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 交易赎楼（视图）
+        public ActionResult TradingForeclosureFloor()
+        {
+            return View();
+        }
+        #endregion
+
+
+
+
     }
 }
